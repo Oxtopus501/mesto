6ce8c8ce3e227f061d862ea05f63e3a78b8e828e
Eugene Korotkov <korotkov-evgenij2009@yandex.ru>
2020-09-18 19:41:20 +0300

    server address changed in api.js

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,33 +1,15 @@
 export class Api {
-    constructor(baseUrl) {
-            this.baseUrl = baseUrl;
+    constructor() {
+
     }
 
     getUserInfo() {
-        /*
-            Можно лучше: не нужно хардкодить адрес сервера и ключ авторизации в каждом запросе
-            Лучше передать их в конструктор и использовать переданные. 
-            При этом итоговый адрес запроса собирается из адреса сервера и адреса ендпоинта:
-            fetch(`${this.baseUrl}/users/me`
-        */
-        return fetch(`${this.baseUrl}/cohort10/users/me`, { //http://praktikum.tk/cohort10/users/me'
+        return fetch('https://nomoreparties.co/cohort10/users/me', {
             headers: {
                 authorization: '7a45e0e5-9273-458c-8551-b84f43589b7b'
             }
         })
         .then((res) => {
-            /*
-            Можно лучше: проверка ответа сервера и преобразование из json
-            дублируется во всех методах класса Api, лучше вынести в отдельный метод:
-                _getResponseData(res) {
-                    if (!res.ok) {
-                        return Promise.reject(`Ошибка: ${res.status}`); 
-                    }
-                    return res.json();
-                }
-            Подчеркивание в начале имени метода говорит о том, что метод является приватным, т.е.
-            не используется вне класса Api   
-            */
             if (res.ok) return res.json();
             return Promise.reject(new Error(res.status));
         })
@@ -35,7 +17,7 @@ export class Api {
     }
 
     getCards() {
-        return fetch(`${this.baseUrl}/cohort10/cards`, {
+        return fetch('https://nomoreparties.co/cohort10/cards', {
             headers: {
                 authorization: '7a45e0e5-9273-458c-8551-b84f43589b7b'
             }
@@ -48,7 +30,7 @@ export class Api {
     }
 
     patchUserInfo(userName, userAbout) {
-        return fetch(`${this.baseUrl}/cohort10/users/me`, {
+        return fetch('https://nomoreparties.co/cohort10/users/me', {
             method: 'PATCH',
             headers: {
                 authorization: '7a45e0e5-9273-458c-8551-b84f43589b7b',
@@ -67,7 +49,7 @@ export class Api {
     }
 
     postNewCard(name, link) {
-        return fetch(`${this.baseUrl}/cohort10/cards`, {
+        return fetch('https://nomoreparties.co/cohort10/cards', {
             method: 'POST',
             headers: {
                 authorization: '7a45e0e5-9273-458c-8551-b84f43589b7b',
